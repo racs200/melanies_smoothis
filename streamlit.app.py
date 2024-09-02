c22eeca9101b25064213d8e2a6afc9d2bd985ae2
racs200 <racs200@gmail.com>
2024-09-01 22:45:28 -0600

    Update streamlit.app.py
    tabs and spaces1

--- streamlit.app.py
+++ streamlit.app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 st.title("Customize your Smoothie! :cup_with_straw:")
 st.write(
@@ -23,8 +24,9 @@ ingredients_list = st.multiselect (
 if ingredients_list:
    ingredients_string = ''
    for fruit_chosen in ingredients_list:
-       ingredients_string += fruit_chosen +' '
-
+           ingredients_string += fruit_chosen +' '
+           fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+           fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
    #st.write(ingredients_string)
 
    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -41,9 +43,5 @@ if ingredients_list:
        st.success('Your Smoothie is ordered!', icon="✅")
 
 
-# New section to desplay fruityvice nutrition information
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#st.text(fruityvice_response).json()
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
